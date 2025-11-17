a8a4b531769e0c5f7c21421ea01ca1e6f68f07dd
mateusnriy <mateusgomesneri@gmail.com>
2025-11-17 12:09:37 -0300

    refactor(segurança): externalizar credenciais e limpar repositório

--- src/main/java/xsolution/Main.java
+++ src/main/java/xsolution/Main.java
@@ -4,11 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main {
     public static void main(String[] args) {
-        String dbUrl = "jdbc:postgresql://localhost/xsolution_db";
-        String dbUser = System.getenv("DB_USER");      
-        String dbPassword = System.getenv("DB_PASSWORD"); 
+
+        Dotenv dotenv = Dotenv.load();
+
+        String dbUrl = dotenv.get("DB_URL");
+        String dbUser = dotenv.get("DB_USER");
+        String dbPassword = dotenv.get("DB_PASSWORD");
 
         if (dbUser == null || dbPassword == null) {
             System.err.println("Erro: As variáveis de ambiente DB_USER e DB_PASSWORD não foram definidas.");
